31abdc345b1affb16222a5151f73bab10572bb9b
tbilbe <tbilbe@gmail.com>
2019-05-20 19:26:43 +0100

    Strings endpoint complete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
-
 const app = express();
+
+
+//Strings endpoint 
+app.get('/strings/Hello/world', (req, res) => {
+  res.status(200).json({
+    "result": 'Hello world!',
+  });
+});
 
 module.exports = app;
--- tests/strings.test.js
+++ tests/strings.test.js
@@ -1,12 +1,14 @@
 describe('/strings', () => {
   describe('GET /hello/{string}', () => {
-    xit('returns "Hello world!" when passed "world"', (done) => {
+    it('returns "Hello world!" when passed "world"', (done) => {
       chai.request(server)
         .get('/strings/hello/world')
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(200);
-          expect(res.body).to.eql({ result: 'Hello world!' });
+          expect(res.body).to.eql({
+            result: 'Hello world!'
+          });
           done();
         });
     });
@@ -19,7 +21,9 @@ describe('/strings', () => {
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(200);
-          expect(res.body).to.eql({ result: 'HELLO' });
+          expect(res.body).to.eql({
+            result: 'HELLO'
+          });
           done();
         });
     });
@@ -32,34 +36,44 @@ describe('/strings', () => {
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(200);
-          expect(res.body).to.eql({ result: 'hello' });
+          expect(res.body).to.eql({
+            result: 'hello'
+          });
           done();
         });
     });
   });
 
   describe('GET /first-characters/{string}', () => {
-    xit('returns the first character of the string when there is no query string', (done) => {
+    xit('returns the first character of the string when there is no query string', (
+    done) => {
       chai.request(server)
         .get('/strings/first-characters/hello')
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(200);
-          expect(res.body).to.eql({ result: 'h' });
+          expect(res.body).to.eql({
+            result: 'h'
+          });
           done();
         });
     });
 
-    xit('returns the first n character of the string when passed a query parameter', (done) => {
-      chai.request(server)
-        .get('/strings/first-characters/sd32fg45')
-        .query({ length: 4 })
-        .end((err, res) => {
-          expect(err).to.equal(null);
-          expect(res.status).to.equal(200);
-          expect(res.body).to.eql({ result: 'sd32' });
-          done();
-        });
-    });
+    xit('returns the first n character of the string when passed a query parameter', (
+      done) => {
+        chai.request(server)
+          .get('/strings/first-characters/sd32fg45')
+          .query({
+            length: 4
+          })
+          .end((err, res) => {
+            expect(err).to.equal(null);
+            expect(res.status).to.equal(200);
+            expect(res.body).to.eql({
+              result: 'sd32'
+            });
+            done();
+          });
+      });
   });
 });
